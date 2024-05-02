16f9ffd63d3abc802e800c5e079e21d320ae2235
김승우 <rnqhqaltjs@naver.com>
2024-05-02 20:43:54 +0900

    ✨ feature : Hilt 의존성 작성

---
+++ app/src/main/java/com/prography/yakgwa/di/ServiceModule.kt
@@ -0,0 +1,20 @@
+package com.prography.yakgwa.di
+
+import com.prography.data.service.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ServiceModule {
+
+    @Provides
+    @Singleton
+    fun providesAuthService(retrofit: Retrofit): AuthService =
+        retrofit.create(AuthService::class.java)
+
+}
