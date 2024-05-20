57f24ef770fad655207f7a5a4485fd658dba4e5a
김승우 <rnqhqaltjs@naver.com>
2024-05-20 15:56:36 +0900

    ✨ feature : 카카오 로그인

--- app/src/main/java/com/prography/yakgwa/ui/login/LoginActivity.kt
+++ app/src/main/java/com/prography/yakgwa/ui/login/LoginActivity.kt
@@ -3,17 +3,10 @@ package com.prography.yakgwa.ui.login
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.kakao.sdk.common.util.Utility
 import com.prography.data.service.KakaoAuthService
 import com.prography.yakgwa.R
 import com.prography.yakgwa.databinding.ActivityLoginBinding
@@ -21,7 +14,6 @@ import com.prography.yakgwa.ui.MainActivity
 import com.prography.yakgwa.util.UiState
 import com.prography.yakgwa.util.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -46,7 +38,7 @@ class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.loginState.collect {
-                    when(it) {
+                    when (it) {
                         is UiState.Loading -> {}
                         is UiState.Success -> navigateToMain()
                         is UiState.Failure -> Timber.tag(TAG).d(it.error)
