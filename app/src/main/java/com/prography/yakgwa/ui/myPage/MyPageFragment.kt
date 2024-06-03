b658e09b851f9f929b084d3e4615036368364bd0
김승우 <rnqhqaltjs@naver.com>
2024-06-03 19:35:15 +0900

    ✏️ update : 로그아웃 기능 이전

--- app/src/main/java/com/prography/yakgwa/ui/myPage/MyPageFragment.kt
+++ app/src/main/java/com/prography/yakgwa/ui/myPage/MyPageFragment.kt
@@ -1,16 +1,60 @@
 package com.prography.yakgwa.ui.myPage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.prography.yakgwa.R
 import com.prography.yakgwa.databinding.FragmentMyPageBinding
+import com.prography.yakgwa.ui.login.LoginActivity
+import com.prography.yakgwa.util.UiState
 import com.prography.yakgwa.util.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MyPageFragment : BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+    private val viewModel: MyPageViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        observer()
+        addListeners()
     }
 
+    private fun observer() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.logoutState.collect {
+                    when (it) {
+                        is UiState.Loading -> {}
+                        is UiState.Success -> {
+                            navigateToAuth()
+                        }
+
+                        is UiState.Failure -> {
+                            Toast.makeText(requireContext(), it.error, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun addListeners() {
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
+    private fun navigateToAuth() {
+        Intent(requireContext(), LoginActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(this)
+        }
+    }
 }
