eb79e429ffc468536fb18e2ef0fab05df08224a9
김승우 <rnqhqaltjs@naver.com>
2024-06-21 17:46:21 +0900

    ♻️ refactor : 일부 meet 로직 -> vote 로직 변경

--- data/src/main/java/com/prography/data/service/MeetService.kt
+++ data/src/main/java/com/prography/data/service/MeetService.kt
@@ -1,14 +1,11 @@
 package com.prography.data.service
 
 import com.prography.data.model.request.RequestCreateMeetDto
-import com.prography.data.model.request.RequestVotePlaceDto
-import com.prography.data.model.request.RequestVoteTimeDto
 import com.prography.data.model.response.BaseResponse
 import com.prography.data.model.response.ResponseCreateMeetDto
 import com.prography.data.model.response.ResponseMeetDetailDto
 import com.prography.data.model.response.ResponseMeetsDto
 import com.prography.data.model.response.ResponseThemesDto
-import com.prography.data.model.response.ResponseTimePlaceDto
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
@@ -39,20 +36,5 @@ interface MeetService {
         @Path("meetId") meetId: Int
     ): BaseResponse<Unit>
 
-    @GET("/api/v1/meets/{meetId}/vote/items")
-    suspend fun getTimePlaceCandidate(@Path("meetId") meetId: Int): BaseResponse<ResponseTimePlaceDto>
 
-    @POST("/api/v1/users/{userId}/meets/{meetId}/vote/schedules")
-    suspend fun voteTime(
-        @Path("userId") userId: Int,
-        @Path("meetId") meetId: Int,
-        @Body requestVoteTimeDto: RequestVoteTimeDto
-    ): BaseResponse<Unit>
-
-    @POST("/api/v1/users/{userId}/meets/{meetId}/vote/places")
-    suspend fun votePlace(
-        @Path("userId") userId: Int,
-        @Path("meetId") meetId: Int,
-        @Body requestVoteTimeDto: RequestVotePlaceDto
-    ): BaseResponse<Unit>
 }
