63952ffcbf7b4c95a3eebe27b118cf0d7ae22cdc
김승우 <rnqhqaltjs@naver.com>
2024-06-21 18:07:51 +0900

    ➕ add : 데이트타임 유틸

---
+++ app/src/main/java/com/prography/yakgwa/util/dateTimeUtils/DateTimeUtils.kt
@@ -0,0 +1,61 @@
+package com.prography.yakgwa.util.dateTimeUtils
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.prography.yakgwa.ui.vote.VotePromiseTimeFragment
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+
+@RequiresApi(Build.VERSION_CODES.O)
+object DateTimeUtils {
+    private val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    private val timeFormatter12Hour = DateTimeFormatter.ofPattern("a hh:mm")
+    private val timeFormatter24Hour = DateTimeFormatter.ofPattern("HH:mm")
+    private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+
+    fun formatDateToString(year: Int, month: Int, day: Int): String {
+        val date = LocalDate.of(year, month, day)
+        return date.format(dateFormatter)
+    }
+
+    fun parseDateFromString(dateString: String?): LocalDate {
+        return dateString?.let {
+            LocalDate.parse(it, dateFormatter)
+        } ?: LocalDate.now()
+    }
+
+    fun formatTimeToString(hour: Int, minute: Int): String {
+        val time = LocalTime.of(hour, minute)
+        return time.format(timeFormatter12Hour)
+    }
+
+    fun parseTimeFromString(timeString: String): LocalTime {
+        return LocalTime.parse(timeString, timeFormatter12Hour)
+    }
+
+    fun formatTimeTo24Hour(timeString: String): String {
+        val localTime = LocalTime.parse(timeString, timeFormatter12Hour)
+        return localTime.format(timeFormatter24Hour)
+    }
+
+    fun parseHourFromTimeString(timeString: String): Int {
+        val localTime = LocalTime.parse(timeString, timeFormatter24Hour)
+        return localTime.hour
+    }
+
+    fun formatLocalDateTimeToString(date: LocalDate, time: LocalTime): String {
+        val dateTime = LocalDateTime.of(date, time)
+        return dateTime.format(dateTimeFormatter)
+    }
+
+    fun LocalDate.toCalendarDay(dayOfMonth: Int = this.dayOfMonth): CalendarDay {
+        return CalendarDay.from(
+            this.year,
+            this.monthValue - VotePromiseTimeFragment.MONTH_OFFSET,
+            dayOfMonth
+        )
+    }
+}
