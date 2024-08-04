0a7c86d4ac8ff663260846b30800ba9bd6fbea8f
김승우 <rnqhqaltjs@naver.com>
2024-08-04 19:30:55 +0900

    ♻️ refactor : 이미지 변환 로직 view -> repository

--- domain/src/main/java/com/prography/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/prography/domain/repository/AuthRepository.kt
@@ -1,7 +1,7 @@
 package com.prography.domain.repository
 
+import android.net.Uri
 import com.prography.domain.model.request.AuthRequestEntity
-import com.prography.domain.model.request.UserImageRequestEntity
 import com.prography.domain.model.response.AuthResponseEntity
 import com.prography.domain.model.response.UserInfoResponseEntity
 
@@ -13,5 +13,5 @@ interface AuthRepository {
 
     suspend fun logout(): Result<Unit>
     suspend fun getUserInfo(): Result<UserInfoResponseEntity>
-    suspend fun updateUserImage(userImageRequestEntity: UserImageRequestEntity): Result<Unit>
+    suspend fun updateUserImage(imageUri: Uri): Result<Unit>
 }
--- domain/src/main/java/com/prography/domain/usecase/PatchUpdateUserImageUseCase.kt
+++ domain/src/main/java/com/prography/domain/usecase/PatchUpdateUserImageUseCase.kt
@@ -1,13 +1,13 @@
 package com.prography.domain.usecase
 
-import com.prography.domain.model.request.UserImageRequestEntity
+import android.net.Uri
 import com.prography.domain.repository.AuthRepository
 
 class PatchUpdateUserImageUseCase(
     private val authRepository: AuthRepository
 ) {
     suspend operator fun invoke(
-        userImageRequestEntity: UserImageRequestEntity
+        imageUri: Uri
     ): Result<Unit> =
-        authRepository.updateUserImage(userImageRequestEntity)
+        authRepository.updateUserImage(imageUri)
 }
