e6fea9f560d656a805576af46435815e7a0269aa
김승우 <rnqhqaltjs@naver.com>
2024-08-19 14:14:09 +0900

    ♻️ refactor : viewModel -> domain 책임 설계

--- domain/src/main/java/com/prography/domain/model/response/TimeCandidateResponseEntity.kt
+++ domain/src/main/java/com/prography/domain/model/response/TimeCandidateResponseEntity.kt
@@ -7,11 +7,16 @@ data class TimeCandidateResponseEntity(
 ) {
     data class TimeInfo(
         val timeId: Int,
-        val voteTime: String
+        val voteTime: String,
+        var isSelected: Boolean = false
     )
 
     data class VoteDate(
         val startVoteDate: String,
         val endVoteDate: String,
     )
+
+    fun selectFirstTime(): List<TimeInfo> = timeInfos?.mapIndexed { index, item ->
+        item.copy(isSelected = index == 0)
+    } ?: emptyList()
 }
--- domain/src/main/java/com/prography/domain/model/response/VotePlaceResponseEntity.kt
+++ domain/src/main/java/com/prography/domain/model/response/VotePlaceResponseEntity.kt
@@ -10,5 +10,10 @@ data class VotePlaceResponseEntity(
         val roadAddress: String,
         val mapX: String,
         val mapY: String,
+        var isSelected: Boolean = false
     )
+
+    fun selectFirstPlace(): List<PlaceInfos> = placeInfos?.mapIndexed { index, item ->
+        item.copy(isSelected = index == 0)
+    } ?: emptyList()
 }
